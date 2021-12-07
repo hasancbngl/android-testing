bdc2405330170ddfd47465f628644a330b5f7f7e
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-12-07 13:59:39 +0300

    create model entity

---
+++ app/src/main/java/com/cobanogluhasan/testing/data/model/Book.kt
@@ -0,0 +1,14 @@
+package com.cobanogluhasan.testing.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "books")
+data class Book(
+    var name: String,
+    var author: String,
+    var year: Int,
+    var imageUrl: String,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+)
