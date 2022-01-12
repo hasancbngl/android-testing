46ee6b88139473146b622ec25026fb1b2a17f669
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-01-12 11:05:42 +0300

    create fake repo

---
+++ app/src/test/java/com/cobanogluhasan/testing/viewmodel/BookViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.cobanogluhasan.testing.viewmodel
+
+import com.cobanogluhasan.testing.ui.viewmodel.BookViewModel
+import org.junit.Before
+
+class BookViewModelTest {
+
+    private lateinit var viewModel : BookViewModel
+
+    @Before
+    fun setup() {
+      //  viewModel = BookViewModel()
+    }
+}
