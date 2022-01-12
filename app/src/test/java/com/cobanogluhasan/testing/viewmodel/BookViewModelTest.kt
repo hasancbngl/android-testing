81ab32eed06366d329faee8e4e28e2bd5e31f0ee
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-01-12 11:10:31 +0300

    commit

--- app/src/test/java/com/cobanogluhasan/testing/viewmodel/BookViewModelTest.kt
+++ app/src/test/java/com/cobanogluhasan/testing/viewmodel/BookViewModelTest.kt
@@ -1,14 +1,15 @@
 package com.cobanogluhasan.testing.viewmodel
 
+import com.cobanogluhasan.testing.repo.FakeBookRepository
 import com.cobanogluhasan.testing.ui.viewmodel.BookViewModel
 import org.junit.Before
 
 class BookViewModelTest {
 
-    private lateinit var viewModel : BookViewModel
+    private lateinit var viewModel: BookViewModel
 
     @Before
     fun setup() {
-      //  viewModel = BookViewModel()
+        viewModel = BookViewModel(FakeBookRepository())
     }
 }
