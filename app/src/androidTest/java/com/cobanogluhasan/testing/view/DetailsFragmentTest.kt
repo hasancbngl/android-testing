f26b301c834e548c5a6fa3cb4f40ceca220ad57c
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-02-02 17:28:37 +0300

    test back functionality and solve bug

--- app/src/androidTest/java/com/cobanogluhasan/testing/view/DetailsFragmentTest.kt
+++ app/src/androidTest/java/com/cobanogluhasan/testing/view/DetailsFragmentTest.kt
@@ -3,10 +3,15 @@ package com.cobanogluhasan.testing.view
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.NoActivityResumedException
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.MediumTest
 import com.cobanogluhasan.testing.R
 import com.cobanogluhasan.testing.data.db.entity.Book
@@ -14,6 +19,7 @@ import com.cobanogluhasan.testing.getOrAwaitValueTest
 import com.cobanogluhasan.testing.launchFragmentInHiltContainer
 import com.cobanogluhasan.testing.repo.FakeBookRepositoryAndroid
 import com.cobanogluhasan.testing.ui.view.CustomFragmentFactory
+import com.cobanogluhasan.testing.ui.view.MainActivity
 import com.cobanogluhasan.testing.ui.view.fragments.DetailsFragment
 import com.cobanogluhasan.testing.ui.view.fragments.DetailsFragmentDirections
 import com.cobanogluhasan.testing.ui.viewmodel.BookViewModel
@@ -39,6 +45,9 @@ class DetailsFragmentTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Inject
     lateinit var fragmentFactory: CustomFragmentFactory
 
@@ -59,6 +68,23 @@ class DetailsFragmentTest {
         verify(navController).navigate(DetailsFragmentDirections.actionDetailsFragmentToSearchApiFragment())
     }
 
+    fun assertMainFragment() {
+
+        onView(ViewMatchers.withId(R.id.fab)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test(expected = NoActivityResumedException::class)
+    fun testOnBackPressed() {
+        assertMainFragment()
+        val navController = mock(NavController::class.java)
+        launchFragmentInHiltContainer<DetailsFragment>(
+            factory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(), navController)
+        }
+        pressBack()
+        verify(navController).popBackStack()
+    }
 
     @Test
     fun testSave() {
